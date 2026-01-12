9389c766e4d109b7c07b2ff5c076512147fdab3d
alzologisticacba <alzologistica@gmail.com>
2026-01-12 08:54:48 -0300

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-import { defineConfig } from "astro/config";
-
-const isProd = process.env.NODE_ENV === "production";
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: "https://alzologisticacba.github.io",
-  base: isProd ? "/alzo-landing/" : "/", // <-- clave: en dev NO hay base, en prod SÍ (con / final)
+  site: 'https://alzologisticacba.github.io',
+  base: '/alzo-landing',
 });
